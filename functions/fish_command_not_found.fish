99ac1577bd027a3624b0bb122288e79f04ecb22b
Tanay Bhomia <bhomia123456789@gmail.com>
2025-05-01 18:02:52 +0530

    refactored code

--- functions/fish_command_not_found.fish
+++ functions/fish_command_not_found.fish
@@ -1,6 +1,6 @@
 function fish_command_not_found
     # === Load config ===
-    set -l freq 3
+    set -l freq 1
     set -l color "random"
     set -l messages
 
@@ -12,13 +12,23 @@ function fish_command_not_found
         set color $COMMENT_COLOR
     end
 
-    # === Load messages ===
+    # === Load messages based on mode ===
     if set -q CMD_NOT_FOUND_MSGS
         set messages $CMD_NOT_FOUND_MSGS
     else if set -q COMMENT_MODE
-        set -l mode_file (dirname (status -f))/../modes/$COMMENT_MODE.txt
-        if test -f $mode_file
-            set messages (cat $mode_file)
+        switch $COMMENT_MODE
+            case "wild"
+                set messages "RAWR! That command was fierce... and wrong!"
+                set -a messages "You're a wild coder! Untamed typos await!"
+                set -a messages "That command went off the rails — love it!"
+                set -a messages "WILD INPUT DETECTED. KEEP GOING!"
+            case "zen"
+                set messages "Breathe. It's just a typo."
+                set -a messages "Calm minds debug best."
+                set -a messages "Let go of the error, keep flowing."
+                set -a messages "One mistake does not define you."
+            case '*'
+                set messages "Oops! But you're still brilliant ✨"
         end
     end
 
